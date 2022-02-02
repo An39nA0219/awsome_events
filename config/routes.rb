3169f03908d482eda56c0218d6ecd896b7d0e0a8
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-02-02 18:28:39 +0900

    ticket create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
-  delete "/logout" => "sessions#destroy"
-  resources :events
+  delete "/logout" => "sessions#destroy", as: :logout
+  
+  resources :events do
+    resources :tickets
+  end
 end
