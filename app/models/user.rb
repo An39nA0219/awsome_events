583f15b959093e732eeaf6833c457ae780857d3a
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-02-02 23:55:49 +0900

    retirements create"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  before_destroy :check_all_events_finished
 
-  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
-  has_many :tickets
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :tickets, dependent: :nullify
+  has_many :participating_events, through: :tickets, source: :event
 
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
@@ -17,5 +19,20 @@ class User < ApplicationRecord
     # find_or_create_byメソッドはActive Recordが用意しているメソッド。
     # 引数で渡したproviderとuidを持つレコードが存在していたら、そのオブジェクトを返すけど、
     # ないならprovider, uid, name, image_urlを設定してレコードを作成して、そのオブジェクトを返してねということ
+  end
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    if created_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "公開中の未終了イベントが存在します。"
+    end
+
+    if participating_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "未終了の参加イベントが存在します。"
+    end
+
+    throw(:abort) unless errors.empty?
   end
 end
